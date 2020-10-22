d936d760cb34db08b43567913ead05a70dbb3ad1
antonchuvirau <anton_chuvirau@icloud.com>
2020-10-22 21:12:23 +0300

    Добавил функцию генерации случайного числа из происзвольного промежутка

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ const messages = ['Всё отлично!', 'В целом всё неплохо
 const fragment = document.createDocumentFragment();
 const parentEl = document.querySelector('.pictures');
 const templateEl = document.querySelector('#picture').content;
+
+function generateNumber (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min)) + min;
+}
