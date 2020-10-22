65fef158b4e5eb281334dc13035f75abf2ff9594
antonchuvirau <anton_chuvirau@icloud.com>
2020-10-22 21:24:53 +0300

    Добавил функцию создания элементов картинок

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@
 
 const NAMES = ['Антон', 'Сергей', 'Дима', 'Павел', 'Иван'];
 const MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-const COUNT = 25;
+const QUANTITY = 25;
+const FRAGMENT = document.createDocumentFragment();
 
-const fragment = document.createDocumentFragment();
 const parentEl = document.querySelector('.pictures');
 const templateEl = document.querySelector('#picture').content;
+const data = generateArray();
 
 function generateNumber(min, max) {
   min = Math.ceil(min);
@@ -18,7 +19,7 @@ function generateNumber(min, max) {
 function generateArray() {
   const results = [];
 
-  for (let i = 0; i <= COUNT; i++) {
+  for (let i = 0; i <= QUANTITY; i++) {
     results.push({
       url: `photos/${i}.jpg`,
       description: `описание фотографии`,
@@ -46,4 +47,13 @@ function generateComments() {
   }
 
   return comments;
+}
+
+function createDOMElements() {
+  for (const item of data) {
+    const pictureEl = fillDOMElement(templateEl.cloneNode(true), item);
+    FRAGMENT.appendChild(pictureEl);
+  }
+  parentEl.appendChild(FRAGMENT);
+  return;
 }
