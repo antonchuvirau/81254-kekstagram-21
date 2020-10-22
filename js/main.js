8e89860047760cc4112fd3ee0b181c8b9c558cf9
antonchuvirau <anton_chuvirau@icloud.com>
2020-10-22 21:31:48 +0300

    Добавил функцию наполнения контентом

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const NAMES = ['Антон', 'Сергей', 'Дима', 'Павел', 'Иван'];
-const MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const NAMES = [`Антон`, `Сергей`, `Дима`, `Павел`, `Иван`];
+const MESSAGES = [`Всё отлично!`, `В целом всё неплохо. Но не всё.`, `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`, `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`, `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`, `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`];
 const QUANTITY = 25;
 const FRAGMENT = document.createDocumentFragment();
 
-const parentEl = document.querySelector('.pictures');
-const templateEl = document.querySelector('#picture').content;
+const parentEl = document.querySelector(`.pictures`);
+const templateEl = document.querySelector(`#picture`).content;
 const data = generateArray();
 
 function generateNumber(min, max) {
@@ -56,4 +56,14 @@ function createDOMElements() {
   }
   parentEl.appendChild(FRAGMENT);
   return;
+}
+
+function fillDOMElement(template, itemData) {
+  const {url, description, comments, likes} = itemData;
+  template.querySelector(`img`).setAttribute(`src`, url);
+  template.querySelector(`img`).setAttribute(`alt`, description);
+  template.querySelector(`.picture__comments`).textContent = comments.length;
+  template.querySelector(`.picture__likes`).textContent = likes;
+
+  return template;
 }
